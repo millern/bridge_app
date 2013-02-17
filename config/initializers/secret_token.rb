11cd43ae42e8c5fb353859ffeb35d651df45b04c
= <njmiller6@gmail.com>
2013-02-16 16:06:44 -0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BridgeApp::Application.config.secret_token = '75add43cfec7edcc1239a97328cd976097ea83040dce2bd74493e18013ef7959e64dc99b8b76f2b99a1754ba0d91defc8ab1ccd9c118d16ac0956017b02cf345'
